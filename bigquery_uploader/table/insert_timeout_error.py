24aa71037d194e437afa07961cf4790c3845b6bd
aizen00x <ankit.thakur01@gmail.com>
2022-01-10 11:51:01 +0200

    Changed error message for insert to BigQuery timeout exception

--- bigquery_uploader/table/insert_timeout_error.py
+++ bigquery_uploader/table/insert_timeout_error.py
@@ -1,3 +1,3 @@
 class InsertTimeoutError(TimeoutError):
     def __init__(self):
-        self.message = "Inserting rows to Big Query table timed out"
+        self.message = "Inserting rows to BigQuery table timed out"
